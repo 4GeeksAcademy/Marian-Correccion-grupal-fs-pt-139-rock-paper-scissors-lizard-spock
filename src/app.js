f025cc839125c3a29b731067df90738d7465e601
mmircea319-dev <mmircea319@gmail.com>
2026-06-19 16:18:31 +0000

    listo

--- src/app.js
+++ src/app.js
@@ -1,3 +1,35 @@
-//write your code here
-console.log("Hello Rigo from the console!");
+const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
+const rules = {
+    rock: ["scissors", "lizard"],
+    paper: ["rock", "spock"],
+    scissors: ["paper", "lizard"],
+    lizard: ["paper", "spock"],
+    spock: ["scissors", "rock"]
+};
+
+const jugar = (userChoice) => {
+
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    const computerChoice = choices[randomIndex];
+
+
+    let mensajeHTML = `<class = bg-succes>You'r choice: <strong>${userChoice}</strong>
+       <br>
+       Sheldon choice: <strong>${computerChoice}</strong>
+       <br><br>`;
+
+
+    if (userChoice === computerChoice) {
+        mensajeHTML += "¡I'ts a tie! 🤝";
+    }
+     else if (rules[userChoice].includes(computerChoice)) {
+        mensajeHTML += "¡You win! 🎉";
+    } 
+    else {
+        mensajeHTML += "Lusser, tray again 😉";
+    }
+
+  
+    document.getElementById("result").innerHTML = mensajeHTML;
+}
