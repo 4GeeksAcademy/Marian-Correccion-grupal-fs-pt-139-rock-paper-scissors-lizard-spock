c7f81217bac0d27a527ff5693db0e3613fc8cedd
mmircea319-dev <mmircea319@gmail.com>
2026-06-22 20:15:35 +0000

    Resolucion de corrección

--- src/app.js
+++ src/app.js
@@ -1,38 +1,37 @@
 const choices = ["rock", "paper", "scissors", "lizard", "spock"];
 
 const rules = {
-    rock: ["scissors", "lizard"],
-    paper: ["rock", "spock"],
-    scissors: ["paper", "lizard"],
-    lizard: ["paper", "spock"],
-    spock: ["scissors", "rock"]
+  rock: ["scissors", "lizard"],
+  paper: ["rock", "spock"],
+  scissors: ["paper", "lizard"],
+  lizard: ["paper", "spock"],
+  spock: ["scissors", "rock"]
 };
 
 const jugar = (userChoice) => {
 
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    const computerChoice = choices[randomIndex];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  const computerChoice = choices[randomIndex];
 
 
-    let mensajeHTML = `<class = bg-succes>You'r choice: <strong>${userChoice}</strong>
+  let mensajeHTML = `<class = bg-succes>You'r choice: <strong>${userChoice}</strong>
        <br>
        Sheldon choice: <strong>${computerChoice}</strong>
        <br><br>`;
 
 
-    if (userChoice === computerChoice) {
-      /*return*/ mensajeHTML += "¡I'ts a tie! 🤝";
-    }
-    if (rules[userChoice].includes(computerChoice)) {
-      /*return*/ mensajeHTML += "¡You win! 🎉";
-    } 
-    if(rules[computerChoice].includes(userChoice)){
-      /*return*/ mensajeHTML += "Lusser, tray again 😉";
-    }
+  if (userChoice === computerChoice) {
+    mensajeHTML += "¡I'ts a tie! 🤝";
+  }
+  if (rules[userChoice].includes(computerChoice)) {
+    mensajeHTML += "¡You win! 🎉";
+  }
+  if (rules[computerChoice].includes(userChoice)) {
+    mensajeHTML += "Lusser, tray again 😉";
+  }
 
-  
-  
-    document.getElementById("result").innerHTML = mensajeHTML;
-}; 
 
-//console.log(jugar("rock"));
+
+  document.getElementById("result").innerHTML = mensajeHTML;
+};
+
